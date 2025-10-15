f895444f95729741f8fb9d328d9e512427d1268e
임기주 <kijoo1128@yonsei.ac.kr>
2025-10-15 13:31:44 +0900

    feat: 로그인 화면 메뉴 버튼 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import LoginPage from "./components/LoginScreen";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginScreen from "./components/LoginScreen";
+import SignupScreen from "./components/SignupScreen";
 
 export default function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-white">
-      {/* 크기 고정 */}
-      <div className="w-[375px] h-[812px] bg-white shadow-md overflow-hidden border border-gray-200">
-        <LoginPage />
+      <div className="w-[23.44rem] h-[50.75rem] bg-white shadow-md overflow-hidden border border-gray-200">
+        <Router>
+          <Routes>
+            {/* 기본 경로로 들어오면 /login으로 자동 이동 */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/signup" element={<SignupScreen />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
